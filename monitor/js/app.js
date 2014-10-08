6804803a758bcb8abdf5c60bba346c129fce4703
Andrew Stewart <andrew@stwrt.ca>
2014-10-08 16:59:09 -0700

    add description of roles in ng app

--- monitor/js/app.js
+++ monitor/js/app.js
@@ -18,8 +18,30 @@ app.config(function($routeProvider) {
     })
 });
 
+// Global EventSource to track events emitted by the 'robot' 
+
 var AttractCtrl = function GameCtrl($scope) {
+  // AttractCtrl should:
+  // - display "Race in the Dreamforce 500 to WIN!"
+  // - display a leaderboard of the top 5 players and their times (data from
+  //   SalesForce)
+  // - display a diagram showing how the demo works.
+  //
+  // Rotate between these three (15 seconds each?) until the game starts
+  // (EventSource receives "gameStarting" event, then redirect to "/game"
 };
 
 var GameCtrl = function GameCtrl($scope) {
+  // GameCtrl should:
+  //
+  // - display countdown as game is about to start (driven by events from game
+  //   itself). This could display as full-background colors, with text
+  //   front-center
+  // - "power meters" (VU meters) for each player (based on events from game,
+  // maybe at 200ms intervals?)
+  // - shows winner after race finished
+  //
+  //
+  // ~10s after the race finishes, should switch back to attract mode (redirect
+  // to /attract
 };
