8d032b119c16cda75ba67370d5ab45de5ac8da03
Adrian Zankich <adrian@hybridgroup.com>
2014-10-22 15:00:52 -0700

    update pump program

--- talk/pump.js
+++ talk/pump.js
@@ -11,11 +11,11 @@ Cylon.config({
 Cylon.api();
 
 Cylon.robot({
-  name: 'pebble',
+  name: 'brewforce',
   served: 0,
   pumping: false,
-  dispenserId: 15,
-  droneId: 5,
+  dispenserId: 5,
+  droneId: 4,
   connections: [
     { name: 'pebble', adaptor: 'pebble' },
     { name: 'edison', adaptor: 'intel-iot' },
@@ -68,51 +68,60 @@ Cylon.robot({
   commands: function() {
     return {
       air_drop: this.airDrop,
-      delivered: this.delivered
+      delivered: this.delivered,
+      report_fault: this.reportFault,
+      activate_pump: this.activatePump
     };
+  },
+  reportFault: function() {
+    console.log("Pushing fault to SF.....");
+    var date = new Date(),
+        dateStr = date.toISOString(),
+        toSend = { dispenserId: this.dispenserId,
+                   drinkId: this.served,
+                   event: 'error',
+                   eventTimestamp: dateStr,
+                   details: 'dispenser'
+        };
+    this.pebble.send_notification("there was an error");
+    this.salesforce.post('/Drink/', toSend, function(err, data) {
+      console.log('Err:', err);
+      console.log('Data:', data);
+    });
+  },
+  activatePump: function() {
+    this.pumping = true;
+    this.pump.digitalWrite(1);
+    this.served += 1;
+    var date = new Date(),
+        dateStr = date.toISOString(),
+        toSend = { 
+          dispenserId: this.dispenserId,
+          drinkId: this.served,
+          event: 'online',
+          eventTimestamp: dateStr,
+          details: 'dispenser'
+        };
+    console.log("Total customers served: " + toSend.drinkId);
+    this.pebble.send_notification("Total customers served: " + toSend.drinkId);
+    this.salesforce.post('/Drink/', toSend, function(err, data) {
+      console.log('Err:', err);
+      console.log('Data:', data);
+    });
+    setTimeout(function() {
+      this.pump.digitalWrite(0);
+      this.pumping = false;
+    }.bind(this), 2000);
   },
   work: function() {
     this.tap.on('press', function() {
       if (!this.pumping) {
-        this.pumping = true;
-        this.pump.digitalWrite(1);
-        this.served += 1;
-        var date = new Date(),
-            dateStr = date.toISOString(),
-            toSend = { dispenserId: this.dispenserId,
-                       drinkId: this.served,
-                       event: 'online',
-                       eventTimestamp: dateStr,
-                       details: 'dispenser'
-        };
-        console.log("Total customers served: " + toSend.drinkId);
-        this.pebble.send_notification("Total customers served: " + toSend.drinkId);
-        this.salesforce.post('/Drink/', toSend, function(err, data) {
-          console.log('Err:', err);
-          console.log('Data:', data);
-        });
-        setTimeout(function() {
-          this.pump.digitalWrite(0);
-          this.pumping = false;
-        }.bind(this), 2000);
+        this.activatePump();
       }
     }.bind(this));
 
     this.fault.on('press', function() {
-      console.log("Pushing fault to SF.....");
-      var date = new Date(),
-          dateStr = date.toISOString(),
-          toSend = { dispenserId: this.dispenserId,
-                     drinkId: this.served,
-                     event: 'error',
-                     eventTimestamp: dateStr,
-                     details: 'dispenser'
-		      };
-      this.pebble.send_notification("there was an error");
-      this.salesforce.post('/Drink/', toSend, function(err, data) {
-        console.log('Err:', err);
-        console.log('Data:', data);
-      });
+      this.reportFault();
     }.bind(this));
   }
 }).start();
